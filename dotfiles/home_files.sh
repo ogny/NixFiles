41955637a2f8a20f3abbae85806332cb9f273cb8
Orkun Gunay <o.gunay@tasit.com>
2014-12-24 15:10:12 +0200

    tmux zsh ve deploy icin home_files'i degistirdik.

---
+++ dotfiles/home_files.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+ln -snf /home/orkung/Git_Repolari/kisisel/public/nixfiles/dotfiles/.tmux.conf ~/
+ln -snf /home/orkung/Git_Repolari/kisisel/public/nixfiles/dotfiles/.muttrc ~/   
+ln -snf ~/Git_Repolari/kisisel/public/nixfiles/bin/girep.sh ~/bin               
+ln -snf /home/orkung/Git_Repolari/kisisel/public/nixfiles/dotfiles/.zsh /home/orkung/                                                                                                                                                          
+ln -snf ~/Git_Repolari/kisisel/public/nixfiles/Vagrantfile ~/                                                                                                                                                                                
+ln -snf /home/orkung/diger/copy/x86_64/CopyConsole /home/orkung/bin         
+ln -snf /home/orkung/Git_Repolari/kisisel/public/nixfiles/dotfiles/.vimrc ~/
+ln -snf /home/orkung/copy/x86_64/CopyConsole ~/bin/
