89954cc136488bcf68cc99bc9b43f3ed7ea01ca4
Orkun Gunay <orkungunayx@gmail.com>
2015-01-22 16:07:32 +0200

    .

--- bin/myxrandr.sh
+++ bin/myxrandr.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-xrandr --output VGA-0 --auto --right-of LVDS-0 --output LVDS-0 --auto
+#xrandr --output VGA-0 --auto --right-of LVDS-0 --output LVDS-0 --auto
+xrandr --output VGA-0 --auto 
 xrandr --output LVDS-0 --auto
